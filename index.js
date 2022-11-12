48e1caed42c71034bbda99e53604f70c9c656839
dinesh24murali <dinesh24murali@gmail.com>
2022-11-12 12:05:34 +0530

    Add: express mongo exporess connection CRUD;

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 
+const postRouter = require("./routes/postRoutes");
+
 const app = express();
 
 
@@ -19,9 +21,13 @@ const connectWithRetry = () => {
 
 connectWithRetry();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
 	res.send("<h2>Hello there</h2>");
 });
+
+app.use("/api/posts", postRouter);
 
 const port = process.env.PORT || 3000;
 
